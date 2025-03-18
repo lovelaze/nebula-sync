f5976333c4983858e56bc5e782db3c7b31c61eec
Fredrik Berntsson <fredrik.kona@gmail.com>
2025-03-18 20:49:39 +0100

    Separate logs to stdout and stderr

--- internal/log/log.go
+++ internal/log/log.go
@@ -3,14 +3,46 @@ package log
 import (
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"io"
 	"os"
 	"strconv"
 	"time"
 )
 
+type LevelWriter struct {
+	io.Writer
+	Levels []zerolog.Level
+}
+
+func (w LevelWriter) WriteLevel(level zerolog.Level, p []byte) (int, error) {
+	for _, l := range w.Levels {
+		if l == level {
+			return w.Write(p)
+		}
+	}
+	return len(p), nil
+}
+
+func newLevelWriter() zerolog.LevelWriter {
+	writer := zerolog.MultiLevelWriter(
+		LevelWriter{
+			Writer: zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339},
+			Levels: []zerolog.Level{
+				zerolog.DebugLevel, zerolog.InfoLevel, zerolog.WarnLevel,
+			},
+		},
+		LevelWriter{
+			Writer: zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339},
+			Levels: []zerolog.Level{
+				zerolog.ErrorLevel, zerolog.FatalLevel, zerolog.PanicLevel,
+			},
+		},
+	)
+	return writer
+}
+
 func Init() {
-	logger := log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339})
-	log.Logger = logger
+	logger := zerolog.New(newLevelWriter()).With().Timestamp().Logger()
 	zerolog.SetGlobalLevel(zerolog.InfoLevel)
 
 	if debugEnv := os.Getenv("NS_DEBUG"); debugEnv != "" {
